410971abc8b0af4ac3a99a3ba9338bc7a69d1abe
Veronica Ni <vni1294@gmail.com>
2023-11-29 22:03:30 -0500

    implemented show_story and show_questions to allow different stories

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import story, excited_story
+
+STORIES = {"silly_story": story, "excited_story": excited_story}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 app.debug = True
 debug = DebugToolbarExtension(app)
 
-@app.get("/questions")
+
+@app.get("/")
 def index():
+    """Show a drop down menu of story templates"""
+
+    return render_template("index.html")
+
+
+@app.get("/questions/<story_url>")
+def show_questions(story_url):
     """Show question prompts with customized input fields for the story"""
+
+    story = STORIES[story_url]
 
     return render_template(
         "questions.html",
-        prompts=silly_story.prompts
+        story_url=story_url,
+        prompts=story.prompts
     )
 
-@app.get("/results")
-def show_story():
+
+@app.get("/results/<story_url>/")
+def show_story(story_url):
     """Displays story on /results page with user inputs from /questions page"""
+    story = STORIES[story_url]
 
-    print(request.args)
+    print(f"request args: {request.args}")
 
-    print(f"This is the template: {silly_story.template}")
+    print(f"This is the template: {story.template}")
 
-    story_result = silly_story.get_result_text(request.args)
+    story_result = story.get_result_text(request.args)
 
     return render_template(
         "results.html",
--- stories.py
+++ stories.py
@@ -37,7 +37,7 @@ class Story:
 
 # Here's a story to get you started
 
-silly_story = Story(
+story = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time, in a long-ago {place}, there lived an exceptionally
        {adjective} {noun}. It loved to {verb} with {plural_noun}."""
